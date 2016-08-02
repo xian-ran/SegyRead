83ef6cbbb895d2cfbd3067a2480ff35dc0ca00a3
YIN MIAO <ymthink@163.com>
2016-08-02 21:04:24 +0800

    modify

--- SegyRead.py
+++ SegyRead.py
@@ -43,14 +43,15 @@ def ReadOneTrace(fileName, trace, sampleNum):
 
 def Wiggle(data, lWidth):
     sampleNum, traceNum = np.shape(data)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     for i in range(traceNum):
         traceData = data[:,i]
         maxVal = np.amax(traceData)
-        plt.plot(i+traceData/maxVal, [j for j in range(sampleNum)], color='black', linewidth=lWidth)
+        ax.plot(i+traceData/maxVal, [j for j in range(sampleNum)], color='black', linewidth=lWidth)
         for a in range(len(traceData)):
             if(traceData[a] < 0):
                 traceData[a] = 0
-        plt.fill(i+traceData/maxVal, [j for j in range(sampleNum)], 'k', linewidth=0)
-    #pylab.grid(True)
-    plt.axis([0,traceNum,sampleNum,0])
+        ax.fill(i+traceData/maxVal, [j for j in range(sampleNum)], 'k', linewidth=0)
+    ax.axis([0,traceNum,sampleNum,0])
     plt.show()
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from ReadSegy import *
+from SegyRead import *
 import numpy as np
-
+import matplotlib.pyplot as plt
+import matplotlib as mpl 
 fileName = '../HorizonTracking/f3_xline300_700inline100_400time4_1848.sgy'
 data = np.zeros([462,200])
 for i in range(200):
     data[:,i] = ReadOneTrace(fileName,i,462)
-
 
 Wiggle(data,0.1)
